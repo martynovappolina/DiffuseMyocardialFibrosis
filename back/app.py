3402093f04a610a2188341df7f65cf96a3bc651b
martynovappolina <polly13042001@mail.ru>
2024-11-12 10:45:34 +0300

    Update app.py

--- back/app.py
+++ back/app.py
@@ -21,14 +21,31 @@ def hello_world():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = {
-        # 'ProBNP': [request.form['ProBNP']],
-        # 'TGF-1b': [request.form['TGF-1b']],
-        # 'LP_mm': [request.form['LP_mm']],
-        'Длительность \nзаболевания\n(количество лет)': [request.form['duration_of_the_disease']],
-        'TGF-1b': [request.form['tgf_1b']],
-        'ProBNP\n(0-125)\nпг\мл': [request.form['nt_proBNP']],
+        'Пол': 0,
+        'Galectin-3': 0,
         'PICP': [request.form['picp']],
+        'PIIINP': 0,
+        'ProBNP\n(0-125)\nпг\мл': [request.form['nt_proBNP']],
+        'TGF-1b': [request.form['tgf_1b']],
+        'Возраст': 0,
+        'Рост': 0,
+        'Вес': 0,
+        'ИМТ кг/м2': 0,
+        'Длительность \nзаболевания\n(количество лет)': [request.form['duration_of_the_disease']],
+        'ИФР-1\n(15-250 нг/мл)': 0,
+        'Ремиссия\n0-нет ремиссии\n1-ремиссия\n': 0,
+        'Нарушение\nуглеводного\nобмена\n1-нет\n2-есть': 0,
+        'АГ степень': 0,
+        'Интервал\nPQ  (мс)': 0,
         'ЛП мм\nдо 40 мм': [request.form['left_atrium']],
+        'ЛП V мл\nдо 69 мл': 0,
+        'МЖП\nдо10 мм': 0,
+        'ЗС': 0,
+        'КДР\nдо 58 мм': 0,
+        'ИММЛЖ\nм до 115г/м2\nж до 95 г/м2': 0,
+        'КДО\n': 0,
+        'КСО': 0,
+        'ФВ%\n(>54)': 0,
     }
 
     df = pd.DataFrame(data)
